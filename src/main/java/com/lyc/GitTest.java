a5bf6e77b966c587db2a18c01219803673193e1d
lyc <18709263035@163.com>
2021-04-26 14:44:07 +0800

    my first commit

---
+++ src/main/java/com/lyc/GitTest.java
@@ -0,0 +1,11 @@
+package com.lyc;
+
+/**
+ * @author lyc
+ * @create 2021--04--26 14:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
